dde43f20f5bc9b8fa99a05cdedf7b4cef6167a7b
Federico Caprari <korsmakolnikov@gmail.com>
2017-11-18 18:02:30 +0100

    partial

--- haskell/src/Katas/KataZero.hs
+++ haskell/src/Katas/KataZero.hs
@@ -14,49 +14,45 @@ import Data.Map as M
 import System.Random
 
 stepA :: [Int]
-stepA = []
+stepA = [1..3]
 
 stepB :: [Int]
-stepB = []
-
-lasciaSoloPari :: [Int] -> [Int]
-lasciaSoloPari xs = []
+stepB = [2, 4..10]
 
 stepC :: [Int]
-stepC = []
+stepC = [7, 7*2..100]
 
 stepD :: [String] -> [String]
-stepD xs =  []
+stepD xs = Data.List.filter (beginWithC) xs
 
-stepE :: Double
-stepE =  0
+beginWithC (c:_) = c == 'c' || c == 'C'
+
+stepE :: Float
+modx x = mod x 8
+multipli = [(fromIntegral u)|u<-[1..100], 0 == (mod u 8)]
+stepE = Data.List.foldl (+) 0 multipli / fromIntegral (length multipli)
 
 stepF :: Int
-stepF = 0
+stepF = Data.List.foldl (+) 0 [u |u<-[6..1000], 0 == mod u 6]
 
 stepG :: [String] -> [String]
-stepG xs = []
+stepG xs = Data.List.sort xs
 
 stepH :: IO Int
-stepH = return (-1) :: IO Int
+stepH = return (41) :: IO Int
 
 stepI :: [String] -> String
-stepI xs = ""
+interv x y = x ++ ", "
+stepI xs = P.foldl (interv) "" xs
 
 stepJ :: [String] -> Set String
-stepJ xs = S.fromList []
+stepJ xs = S.fromList xs
 
 stepK :: [String] -> Map Int String
 stepK xs = M.fromList []
 
 stepL :: [String] -> [Int]
-stepL xs = []
-
-stepL2 :: [String] -> [Int]
-stepL2 xs = []
+stepL xs = Data.List.map (length) xs
 
 stepM :: [String] -> [String]
-stepM xs = []
-
-stepM2 :: [String] -> [String]
-stepM2 xs = []
+stepM xs = P.map (take 1) xs
